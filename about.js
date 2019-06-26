35ae66731106c146f6b28240ebcc745c48e4fb0a
Vanesa Hernandez Flores <fleurs@Vanesas-MacBook-Pro.local>
2019-06-26 16:04:28 -0500

    still working on about us JS and less files

--- about.js
+++ about.js
@@ -9,9 +9,16 @@ class Tab {
     constructor(tabElement){
         this.element = tabElement;
         this.tabNum = this.element.dataset.tab;
+        this.about = document.querySelectorAll(`.about[data-tab="${this.tabNum}"]`);
+        this.about= Array.from(this.about).map(aboutItem => new Content(aboutItem));
+        this.element.addEventListener('click', () => this.dropContent());
     }
     // this.contentElement = document.querySelector(`div.about[data-tab="${this.tabNum}"]`);
-    
+    //functions go here! -Ryan
+    dropContent(event) {
+        let tab = querySelectorAll(".link");
+        
+    }
 }
 
 let tabs = document.querySelectorAll(".link");
